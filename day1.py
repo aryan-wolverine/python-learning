978e51b2828d7d162cdb61d44320294dc74e1d51
Ubuntu <ubuntu@ip-192-168-7-69.ap-south-1.compute.internal>
2020-05-23 17:44:55 +0530

    day1

---
+++ day1.py
@@ -0,0 +1,30 @@
+#print("Hello World")
+#name = "Aryan"
+#print("Hello " + name)
+#name = "Shija"
+#print("Hello " + name)
+
+# addition
+var1 = 5.1
+var2 = 10.1
+output = var1 + var2
+print("sum ", output)
+
+# subtraction
+var1 = 5
+var2 = 15
+output = var2 - var1
+print("bal ", output)
+
+# multiplication
+var1 = 5
+var2 = 15
+output = var1 * var2
+print("product ", output)
+
+# division
+var1 = 5
+var2 = 15
+output = var2/var1
+print("quotient ", output)
+
