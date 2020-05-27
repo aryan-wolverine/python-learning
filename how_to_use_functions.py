596ec541e987c2397fcd96adba9e7b63dbe84121
Ubuntu <ubuntu@ip-192-168-7-69.ap-south-1.compute.internal>
2020-05-27 12:47:21 +0530

    Function Example

---
+++ how_to_use_functions.py
@@ -0,0 +1,13 @@
+def this_is_afunction():
+    print("Hello, you just called the function")
+
+
+# call the function
+this_is_afunction()
+
+
+
+def this_is_a_function_with_parameter(name):
+    print("Hello, ", name)
+
+this_is_a_function_with_parameter("Aryan")
